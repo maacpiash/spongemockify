89a22448138fa9a4ac052c244640e14c9b4244b9
Piash at Home on Linux Mint <maacpiash@outlook.com>
2019-02-22 00:13:19 +0600

    Alhamdu lillah done with first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# SpongeMockify
+
+This simple Node.js module takes a string as a CLI input and converts it into a [mocking Spongebob meme](https://knowyourmeme.com/memes/mocking-spongebob) style text.
+
+It follows one simple rules: no more than three consecutive same-case (uppercase/lowercase) characters.
+
+## Installation
+
+#### As a Node module
+A simple `npm install spongemockify` inside your project folder should do the job.
+
+#### As a CLI tool
+From any folder, execute `npm install -g spongemockify` command. Superuser access may be needed in Unix/Linux systems.
+
+## Usage
+As a CLI tool:
+```bash
+spongemockify <string>
+```
+
+Enjoy!
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function spongemockify(str) {
+  let rand = 0;
+
+  let upperCount = 0; // counts consecutive uppercase characters
+  let lowerCount = 0; // counts consecutive lowercase characters
+  
+  let characters = str.split('');
+
+  let newChars = [];
+
+  for (let c of characters) {
+    if (upperCount == 2) {
+      newChars.push(c.toLowerCase());
+      upperCount = 0;
+      lowerCount = 1;
+    } else if (lowerCount == 2) {
+      newChars.push(c.toUpperCase());			
+      lowerCount = 0;
+      upperCount = 1;
+    } else { // upperCount <= 1 && lowerCount <= 1
+      rand = Math.floor(Math.random() * 2); // either 0 or 1 only
+      newChars.push(rand ? c.toUpperCase() : c.toLowerCase());
+      if (rand)
+        upperCount++; 
+      else
+        lowerCount++;
+    }
+  }
+
+  return newChars.join('');
+}
+
+console.log(spongemockify(process.argv[2]));
+
+module.exports = { spongemockify };
