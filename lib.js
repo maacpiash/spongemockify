96190676c49ac200df5b1e0ac4ab9914da200f76
Shawon Ashraf <shawon13@live.com>
2019-02-23 00:38:13 +0600

    removed strict and fixed equality operator

--- lib.js
+++ lib.js
@@ -1,29 +1,28 @@
-'use strict';
-
 function spongemockify(str) {
   let rand = 0;
 
   let upperCount = 0; // counts consecutive uppercase characters
   let lowerCount = 0; // counts consecutive lowercase characters
-  
+
   let characters = str.split('');
 
   let newChars = [];
 
   for (let c of characters) {
-    if (upperCount == 2) {
+    if (upperCount === 2) {
       newChars.push(c.toLowerCase());
       upperCount = 0;
       lowerCount = 1;
-    } else if (lowerCount == 2) {
-      newChars.push(c.toUpperCase());			
+    } else if (lowerCount === 2) {
+      newChars.push(c.toUpperCase());
       lowerCount = 0;
       upperCount = 1;
-    } else { // upperCount <= 1 && lowerCount <= 1
+    } else {
+      // upperCount <= 1 && lowerCount <= 1
       rand = Math.floor(Math.random() * 2); // either 0 or 1 only
       newChars.push(rand ? c.toUpperCase() : c.toLowerCase());
       if (rand)
-        upperCount++; 
+        upperCount++;
       else
         lowerCount++;
     }
